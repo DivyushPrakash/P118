63c10fb4d6b66a9c137fe95f778013ce664f774e
DivyushPrakash <84733784+DivyushPrakash@users.noreply.github.com>
2022-01-28 22:58:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+timecounter = 0;
+timecheck = "";
+drawsketch = "";
+answerholder = "";
+score = 0;
+
+function setup() {
+    canvas = createCanvas(600, 400);
+    canvas.center();
+    background("white");
+}
